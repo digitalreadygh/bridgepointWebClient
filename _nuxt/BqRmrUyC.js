92c6b250c509867d816a54902424d5ddff3ffc1e
iamtomc <brandsafric@gmail.com>
2025-02-04 12:26:23 +0000

    Build: (578e909) borderlines for adjusting removed

---
+++ _nuxt/BqRmrUyC.js
@@ -0,0 +1,1 @@
+import{r as s,o as a,c as o,D as r}from"./Dz-nsm6z.js";const t={class:"max-w-screen-xl mx-auto px-5"},l={__name:"Container",setup(n){return s(!1),(e,c)=>(a(),o("div",t,[r(e.$slots,"default")]))}};export{l as _};
