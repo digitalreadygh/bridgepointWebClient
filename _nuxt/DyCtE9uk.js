e218f8c399df5504739cd0eb8b82bc9506f9d4f6
iamtomc <brandsafric@gmail.com>
2025-02-04 12:23:07 +0000

    Build: (3db3927) contact

--- _nuxt/DyCtE9uk.js
+++
@@ -1,1 +0,0 @@
-import{r as s,o as a,c as o,D as r}from"./bX1scWA0.js";const t={class:"max-w-screen-xl mx-auto px-5"},l={__name:"Container",setup(n){return s(!1),(e,c)=>(a(),o("div",t,[r(e.$slots,"default")]))}};export{l as _};
